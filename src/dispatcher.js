69b4d1186eba8de926d589fbc7a44aef8c26f05c
ushiboy <ushiboy.dev@gmail.com>
2016-04-14 00:01:21 +0900

    draft

---
+++ src/dispatcher.js
@@ -0,0 +1,21 @@
+import EventEmitter from './event';
+
+export default class Dispatcher extends EventEmitter {
+
+  constructor(scriptPath) {
+    super();
+    this._worker = new Worker(scriptPath);
+    this._worker.onmessage = evt => {
+      const { state } = JSON.parse(evt.data);
+      /**
+       * FIXME Dispatcherではなくなっているような..
+       */
+      this.emit('change', state);
+    };
+  }
+
+  dispatch(action) {
+    this._worker.postMessage(JSON.stringify(action));
+  }
+
+}
